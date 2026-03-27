191a2d301e52fc9a7656e8b50dee033411dee70d
Edward Astill <edwardast.ll@gmail.com>
2026-03-27 18:51:22 +0800

    feat: expose new library shapes in top-level package

--- src/sectiony/__init__.py
+++ src/sectiony/__init__.py
@@ -1,5 +1,9 @@
 from .geometry import Geometry, Contour, Line, Arc, CubicBezier
 from .section import Section
 from .stress import Stress
+from .library import chs, rhs, i, u, solid_rect, solid_circle, shs, angle, t_section
 
-__all__ = ["Section", "Geometry", "Contour", "Line", "Arc", "CubicBezier", "Stress"]
+__all__ = [
+    "Section", "Geometry", "Contour", "Line", "Arc", "CubicBezier", "Stress",
+    "chs", "rhs", "i", "u", "solid_rect", "solid_circle", "shs", "angle", "t_section",
+]
